bf764b26370778bec2327864c06bcbc6435359ea
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-02 15:05:39 -0400

    calling on live application, foot-rosters to serve up JSON to sports app

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,8 @@
 class TeamsController < ApplicationController
+  require 'open-uri'
+  
   def index
+    @roster_url = "https://foot-rosters.herokuapp.com/teams"
+    @teams = JSON.load(open(@roster_url))
   end
 end
