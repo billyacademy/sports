2228b3e3143815dee5ce6bd9453ec8e1236e2696
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-05 16:48:05 -0400

    added helpers to reduce some redundancy in code across controllers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def conference_url
+    "https://foot-rosters.herokuapp.com/conferences"
+  end
+
+  def division_url
+    "https://foot-rosters.herokuapp.com/divisions"
+  end
+
+  def team_url
+    "https://foot-rosters.herokuapp.com/teams"
+  end
+
+  def roster_url
+    "https://foot-rosters.herokuapp.com/teams"
+  end
 end
