78adf2d362deca3083a6be4fd848b5ae9de05c95
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-05 13:40:41 -0400

    added index and show to divisions controller

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -5,4 +5,14 @@ class DivisionsController < ApplicationController
     @division_url = "https://foot-rosters.herokuapp.com/divisions"
     @divisions = JSON.load(open(@division_url))
   end
+
+  def show
+    @conference_url = "https://foot-rosters.herokuapp.com/conferences"
+    @division_url = "https://foot-rosters.herokuapp.com/divisions"
+    @team_url = "https://foot-rosters.herokuapp.com/teams"
+
+    @conferences = JSON.load(open(@conference_url))["conferences"]
+    @divisions = JSON.load(open(@division_url))
+    @teams = JSON.load(open(@team_url))
+  end
 end
