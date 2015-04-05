e6b233af788ca63bdc2f21e7b634672ae4c03f77
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-05 13:39:14 -0400

    added a show method to the conferences controller, have some duplicate code here

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -5,4 +5,11 @@ class ConferencesController < ApplicationController
     @conference_url = "https://foot-rosters.herokuapp.com/conferences"
     @conferences = JSON.load(open(@conference_url))["conferences"]
   end
+
+  def show
+    @conference_url = "https://foot-rosters.herokuapp.com/conferences"
+    @division_url = "https://foot-rosters.herokuapp.com/divisions"
+    @conferences = JSON.load(open(@conference_url))["conferences"]
+    @divisions = JSON.load(open(@division_url))
+  end
 end
