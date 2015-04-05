c9bb988bb5693d7fd099b021dd56d2ea6ce8a963
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-05 15:55:34 -0400

    updated teams_controller with show method

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,8 +1,18 @@
 class TeamsController < ApplicationController
   require 'open-uri'
-  
+
   def index
     @roster_url = "https://foot-rosters.herokuapp.com/teams"
     @teams = JSON.load(open(@roster_url))
+  end
+
+  def show
+    @conference_url = "https://foot-rosters.herokuapp.com/conferences"
+    @division_url = "https://foot-rosters.herokuapp.com/divisions"
+    @team_url = "https://foot-rosters.herokuapp.com/teams"
+
+    @conferences = JSON.load(open(@conference_url))["conferences"]
+    @divisions = JSON.load(open(@division_url))
+    @teams = JSON.load(open(@team_url))
   end
 end
